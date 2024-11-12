349a1923c61deca634ce76d616934f64d7fb7e1b
swathigowda93 <swathigowda95430@gmail.com>
2024-11-12 10:50:21 +0530

    Initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,16 @@
+public class LinkedList {
+    Node head;
+
+    LinkedList() {
+        head = null;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+}
+
+
